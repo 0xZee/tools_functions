06fe3b043b27266a8604e80f5bc084d6f5f260c5
0xZee <91375714+0xZee@users.noreply.github.com>
2024-12-22 18:19:19 +0100

    Update yfinance_stock_data.py

--- yfinance_stock_data.py
+++ yfinance_stock_data.py
@@ -15,7 +15,8 @@ def get_stock_data(tickers, list_keys):
   return data
 
 tickers=["PLTR","IONQ", "NVDA", "ASTS", "RKLB", "QBTS", "RXRX", "AMD", "LAES", "GSAT", "LUNR"]
+tickers_list = ["VHAI", "AVGO", "LUNR", "TLRY", "PL", "DQ", "OLO", "QBTS", "NTLA", "TSLA", "FSLY", "SDGR", "META", "TXG", "TDOC", "APLD", "ENPH", "PSN", "BYDDY", "MRVL", "EXAS", "XPEV", "NKE", "MU", "RBRK", "RKLB", "NKLA", "EDIT", "PANW", "OKTA", "RIVN", "NEE", "RTX", "ARM", "NTNX", "PLTR", "QCOM", "GE", "KC", "SMR", "GOOGL", "BEAM", "AMZN", "RGTI", "QS", "OKLO", "RXRX", "MDB", "PSTG", "AMD", "BABA", "GRAB", "SMCI", "IBM", "AFRM", "VLN", "NOW", "CSCO", "ADBE", "ACB", "IOT", "KO", "ARQQ", "MSFT", "NVDA", "ACHR", "AAPL", "CRSP", "ZS", "BKSY", "NET", "GSAT", "BRZE", "AVAV", "NNOX", "COST", "CGC", "VST", "ORCL", "AI", "VKTX", "BEPC", "BE", "LLAP", "DDOG", "SNOW", "CHPT", "DNA", "SOUN", "ASTS", "JD", "SERV", "PACB", "IONQ", "S", "PATH", "BBAI", "FTNT", "WMT", "COIN", "QUBT", "BHAT", "RDW", "INTC", "GTLB", "AES", "NIO", "CRWD", "RR", "PYPL", "NNE", "QMCO"]
 to_drop = ['address1', 'address2', 'industryDisp', 'industryKey', 'companyOfficers']
 list_keys = ['symbol', 'currentPrice','marketCap', 'trailingPE', 'forwardPE', 'priceToSalesTrailing12Months', 'shortRatio', 'enterpriseToRevenue', 'enterpriseToEbitda', 'targetHighPrice', 'returnOnEquity' ]
-stock_data = get_stock_data(tickers, list_keys)
+stock_data = get_stock_data(tickers_list, list_keys)
 stock_data[0]
