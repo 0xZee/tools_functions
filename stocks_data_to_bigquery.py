87ba23e3e9f5130ab59418519d04d619a560da7e
0xZee <91375714+0xZee@users.noreply.github.com>
2024-12-27 16:41:15 +0100

    Update stocks_data_to_bigquery.py

--- stocks_data_to_bigquery.py
+++ stocks_data_to_bigquery.py
@@ -69,11 +69,13 @@ table_name = 'table_stx_202412'        # Replace with your desired table name
 table_id = f"{project_id}.{dataset_name}.{table_name}"
 
 # run ingesting job
-job = client.load_table_from_dataframe(dataset, table_id)
+#job = client.load_table_from_dataframe(dataset, table_id)
+#job.result()  # Wait for the job to complete
 
-job.result()  # Wait for the job to complete
+# upload df to table BigQuery
+dataset.to_gbq(destination_table=table_id, project_id=project_id, if_exists='replace',  credentials=None)
 
 # check table info
 table = client.get_table(table_id)
-print(f"Loaded {table.num_rows} rows to {table_id}")
-print(table.schema)
+print(f"# Loaded {table.num_rows} rows to Table : {table_id}")
+print(f"# Table Schema : {table.schema}")
