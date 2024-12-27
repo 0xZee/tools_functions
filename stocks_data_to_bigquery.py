34dc3aa3555c32bf408caa142e576db4a9981282
0xZee <91375714+0xZee@users.noreply.github.com>
2024-12-27 16:27:34 +0100

    Update stocks_data_to_bigquery.py
    update List_tickers

--- stocks_data_to_bigquery.py
+++ stocks_data_to_bigquery.py
@@ -30,7 +30,23 @@ def get_stock_data(tickers_list, keys_list):
 
 
 # Example usage:
-tickers_list = ["AVGO", "LUNR", "TLRY", "PL", "DQ", "OLO", "QBTS", "NTLA", "TSLA", "FSLY", "SDGR", "META", "TXG", "TDOC", "APLD", "ENPH", "PSN", "BYDDY", "MRVL", "EXAS", "XPEV", "NKE", "MU", "RBRK", "RKLB", "NKLA", "EDIT", "PANW", "OKTA", "RIVN", "NEE", "RTX", "ARM", "NTNX", "PLTR", "QCOM", "GE", "KC", "SMR", "GOOGL", "BEAM", "AMZN", "RGTI", "QS", "OKLO", "RXRX", "MDB", "PSTG", "AMD", "BABA", "GRAB", "SMCI", "IBM", "AFRM", "VLN", "NOW", "CSCO", "ADBE", "ACB", "IOT", "KO", "ARQQ", "MSFT", "NVDA", "ACHR", "AAPL", "CRSP", "ZS", "BKSY", "NET", "GSAT", "BRZE", "AVAV", "NNOX", "COST", "CGC", "VST", "ORCL", "AI", "VKTX", "BEPC", "BE", "LLAP", "DDOG", "SNOW", "CHPT", "DNA", "SOUN", "ASTS", "JD", "SERV", "PACB", "IONQ", "S", "PATH", "BBAI", "FTNT", "WMT", "COIN", "QUBT", "BHAT", "RDW", "INTC", "GTLB", "AES", "NIO", "CRWD", "RR", "PYPL", "NNE", "QMCO", "LAES", ]
+tickers_list = [
+    'AAPL', 'NVDA', 'MSFT', 'GOOGL', 'AMZN', 'META', 'TSLA', 'AVGO', 'WMT',
+    'ORCL', 'COST', 'NFLX', 'CRM', 'KO', 'CSCO', 'NOW', 'IBM', 'DIS', 'BABA',
+    'AMD', 'ADBE', 'GE', 'PLTR', 'QCOM', 'RTX', 'NEE', 'ANET', 'ARM', 'UBER',
+    'PANW', 'NKE', 'BYDDY', 'MU', 'MRVL', 'SPOT', 'INTC', 'PYPL', 'CRWD',
+    'MSTR', 'FTNT', 'WDAY', 'CEG', 'COIN', 'JD', 'SQ', 'SNOW', 'DDOG', 'VST',
+    'NET', 'RDDT', 'ZS', 'IOT', 'ALAB', 'PSTG', 'AFRM', 'GRAB', 'MDB', 'SMCI',
+    'NTNX', 'OKTA', 'RIVN', 'RKLB', 'RBRK', 'XPEV', 'MNDY', 'ROKU', 'EXAS',
+    'PSN', 'IONQ', 'ENPH', 'GTLB', 'NIO', 'AES', 'SOUN', 'S', 'PATH', 'ASTS',
+    'LYFT', 'ACHR', 'BE', 'BEPC', 'PONY', 'VKTX', 'AI', 'RGTI', 'AVAV', 'BRZE',
+    'GSAT', 'CRSP', 'OKLO', 'QS', 'KC', 'RXRX', 'QBTS', 'QUBT', 'BEAM', 'SMR',
+    'APLD', 'TXG', 'LUNR', 'TDOC', 'SDGR', 'FSLY', 'TLRY', 'PL', 'DQ', 'OLO',
+    'NTLA', 'RDW', 'KULR', 'BBAI', 'NNE', 'RZLV', 'SERV', 'LAES', 'DNA', 'PACB',
+    'ARQQ', 'CHPT', 'QSI', 'NNOX', 'CGC', 'QMCO', 'SPIR', 'BKSY', 'RR', 'VLN',
+    'ACB', 'QNCCF', 'EDIT', 'NKLA', 'LLAP', 'MDAI'
+]
+tickers_list_old = ["AVGO", "LUNR", "TLRY", "PL", "DQ", "OLO", "QBTS", "NTLA", "TSLA", "FSLY", "SDGR", "META", "TXG", "TDOC", "APLD", "ENPH", "PSN", "BYDDY", "MRVL", "EXAS", "XPEV", "NKE", "MU", "RBRK", "RKLB", "NKLA", "EDIT", "PANW", "OKTA", "RIVN", "NEE", "RTX", "ARM", "NTNX", "PLTR", "QCOM", "GE", "KC", "SMR", "GOOGL", "BEAM", "AMZN", "RGTI", "QS", "OKLO", "RXRX", "MDB", "PSTG", "AMD", "BABA", "GRAB", "SMCI", "IBM", "AFRM", "VLN", "NOW", "CSCO", "ADBE", "ACB", "IOT", "KO", "ARQQ", "MSFT", "NVDA", "ACHR", "AAPL", "CRSP", "ZS", "BKSY", "NET", "GSAT", "BRZE", "AVAV", "NNOX", "COST", "CGC", "VST", "ORCL", "AI", "VKTX", "BEPC", "BE", "LLAP", "DDOG", "SNOW", "CHPT", "DNA", "SOUN", "ASTS", "JD", "SERV", "PACB", "IONQ", "S", "PATH", "BBAI", "FTNT", "WMT", "COIN", "QUBT", "BHAT", "RDW", "INTC", "GTLB", "AES", "NIO", "CRWD", "RR", "PYPL", "NNE", "QMCO", "LAES", ]
 keys_list = ['symbol', 'shortName', 'country','industry','sector','currentPrice','marketCap', 'trailingPE', 'forwardPE', 'priceToSalesTrailing12Months', 'priceToBook', 'debtToEquity','shortRatio', 'enterpriseToRevenue', 'enterpriseToEbitda', 'beta', 'fiftyTwoWeekHigh', 'fiftyTwoWeekLow', 'targetMeanPrice', 'targetHighPrice', 'recommendationKey','returnOnEquity', 'totalRevenue', 'freeCashflow', 'totalDebt', 'earningsGrowth', 'revenueGrowth', 'grossMargins', 'ebitdaMargins', 'operatingMargins', 'profitMargins','trailingPegRatio']
 
 # Get data
